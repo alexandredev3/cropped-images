7a6f36fc5c26d29c59b715b9b455efe085203e37
alexandredev3 <costasalexandre511@gmail.com>
2020-10-01 20:36:37 -0300

    added click on select image

---
+++ scripts.js
@@ -0,0 +1,4 @@
+document.getElementById('select-image').onclick = function () {
+  // eu estou pegando o input com o id "photo-file" e dando um clique.
+  document.getElementById('photo-file').click();
+};
